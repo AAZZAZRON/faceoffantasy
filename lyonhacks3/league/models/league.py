c4025ba6fa77ae4e03ffddc7cb6827ee8ba923cb
Daniel Ye <danielyeqt@gmail.com>
2023-04-07 16:05:07 -0400

    create player methods

--- lyonhacks3/league/models/league.py
+++ lyonhacks3/league/models/league.py
@@ -2,10 +2,14 @@ from django.db import models
 from .users import Hockey_User
 from players.models import Player
 
-class League(models.Model):
-    name = models.CharField(max_length=30)
-
 class Team(models.Model):
     user = models.ForeignKey(Hockey_User, on_delete=models.CASCADE)
-    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    league = models.ForeignKey("League", on_delete=models.CASCADE)
     players = models.ManyToManyField(Player)
+
+class League(models.Model):
+    name = models.CharField(max_length=30)
+    def player_score(self, player: Player):
+        return player.assists
+    def team_score(self, team: Team):
+        return sum([self.player_score(i) for i in team.players.all()])
--- lyonhacks3/players/models.py
+++ lyonhacks3/players/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class NHLTeam(models.Model):
+    # as per https://statsapi.web.nhl.com/api/v1/teams/6
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=200)
+    teamName = models.CharField(max_length=200)
+    locationName = models.CharField(max_length=200)
+    shortName = models.CharField(max_length=200)
+    officialSiteUrl = models.CharField(max_length=200)
+    division = models.CharField(max_length=200)
+    conference = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    code = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=200)
+    def __str__(self):
+        return self.type
+
 class Player(models.Model):
     # player info as per https://statsapi.web.nhl.com/api/v1/people/8477956/
     id = models.IntegerField(primary_key=True)
@@ -22,9 +45,8 @@ class Player(models.Model):
     rookie = models.BooleanField()
     shootsCatches = models.CharField(max_length=200)
     rosterStatus = models.CharField(max_length=200)
-    currentTeam = models.CharField(max_length=200)
-    currentTeam = models.ForeignKey('NHLTeam', on_delete=models.CASCADE, null=True)
-    primaryPosition = models.ForeignKey('Position', on_delete=models.CASCADE, null=True)
+    currentTeam = models.ForeignKey(NHLTeam, on_delete=models.CASCADE, null=True)
+    primaryPosition = models.ForeignKey(Position, on_delete=models.CASCADE, null=True)
 
     # player stats as per https://statsapi.web.nhl.com/api/v1/people/8477956/stats?stats=statsSingleSeason&season=20222023
     timeOnIce = models.CharField(max_length=200)
@@ -56,27 +78,4 @@ class Player(models.Model):
     powerPlayTimeOnIcePerGame = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
-
-class Position(models.Model):
-    code = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    type = models.CharField(max_length=200)
-    abbreviation = models.CharField(max_length=200)
-    def __str__(self):
-        return self.type
-
-class NHLTeam(models.Model):
-    # as per https://statsapi.web.nhl.com/api/v1/teams/6
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
-    link = models.CharField(max_length=200)
-    abbreviation = models.CharField(max_length=200)
-    teamName = models.CharField(max_length=200)
-    locationName = models.CharField(max_length=200)
-    shortName = models.CharField(max_length=200)
-    officialSiteUrl = models.CharField(max_length=200)
-    division = models.CharField(max_length=200)
-    conference = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
+        return self.firstName + ' ' + self.lastName

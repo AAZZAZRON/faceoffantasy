56970aba2c0273442f6f1875cae52739316f63a1
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-04-08 17:18:36 -0400

    list players
    css too hard :sob:

--- frontend/src/app/screens/playersScreen.js
+++ frontend/src/app/screens/playersScreen.js
@@ -7,11 +7,15 @@ export default function PlayersScreen (props) {
 
     const [skaters, setSkaters] = useState([]);
     const [goalies, setGoalies] = useState([]);
+    const [positions, setPositions] = useState([]);
+    const [NHLTeams, setNHLTeams] = useState([]);
     const [loaded, setLoaded] = useState(false);
 
     const onStartup = async() => {
         setSkaters(await getDataCache("SKATERS"));
         setGoalies(await getDataCache("GOALIES"));
+        setPositions(await getDataCache("POSITIONS"));
+        setNHLTeams(await getDataCache("NHLTEAMS"));
         setLoaded(true);
     }
 
@@ -21,14 +25,65 @@ export default function PlayersScreen (props) {
 
     return (
         <>
-            {loaded ? <div class="playerHeader">Skaters</div> : <div class="playerHeader">Loading...</div>}
-            <ul class="player-list">
-                {skaters.map((skater, index) => (
-                    <li key={index} class="player">
-                        <div class="playerName">{skater.firstName + ' ' + skater.lastName}</div>
-                    </li>
-                ))}
-            </ul>
+            <div class="player-container">
+                {loaded ? 
+                <div class="card" style={{flexDirection: 'row'}}>
+                    <div class='card-header'>Player</div>
+                    <div class='card-body'>
+                        <div>GP</div>
+                        <div>G</div>
+                        <div>A</div>
+                        <div>PIM</div>
+                        <div>PPP</div>
+                        <div>SHG</div>
+                        <div>SOG</div>
+                        <div>HIT</div>
+                        <div>BLK</div>
+                        <div>+/-</div>
+                    </div>
+                </div>
+                 : 
+                 <div class="playerHeader">Loading...</div>}
+                {skaters.map((skater, index) => {
+                    const position = positions.find(position => position.id === skater.primaryPosition);
+                    const team = NHLTeams.find(team => team.id === skater.currentTeam);
+                    return (
+                        <SkaterCard key={index} skater={skater} position={position} team={team}></SkaterCard>
+                    )
+                    })
+                }
+            </div>
         </>
     );
+}
+
+function SkaterCard(props) {
+    const skater = props.skater;
+    console.log(props.team);
+    return (
+        <div class="card" style={{flexDirection: 'row'}}>
+            <div class="card-header">
+                <img src={skater.avatar} alt="headshot" class="headshot"/>
+                <div>
+                    <div class="name">{skater.firstName} {skater.lastName}</div>
+                    <div class="card-header-bottom">
+                        <div class="team">{props.team.abbreviation}</div>
+                        <div class="position">{props.position.abbreviation}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div>{skater.games}</div>
+                <div>{skater.goals}</div>
+                <div>{skater.assists}</div>
+                <div>{skater.pim}</div>
+                <div>{skater.powerPlayPoints}</div>
+                <div>{skater.shortHandedPoints}</div>
+                <div>{skater.shots}</div>
+                <div>{skater.hits}</div>
+                <div>{skater.blocked}</div>
+                <div>{skater.plusMinus}</div>
+            </div>
+        </div>
+    )
 }

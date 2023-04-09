461bc86fe0510d7438d5aa519a048aa09198edd4
mars-flat <shanec2k05@gmail.com>
2023-04-09 14:42:49 -0400

    test

--- frontend/src/app/screens/leagueSwitchScreen.js
+++ frontend/src/app/screens/leagueSwitchScreen.js
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import "../../css/leagueSwitchScreen.css";
+import Routes from '../utils/misc/routes';
 
 export default function LeagueSwitchScreen(props) {
     
@@ -46,7 +47,6 @@ function LeagueCreationModal(props) {
     const [modalIsOpen , setModalIsOpen] = React.useState(false);
 
     useEffect(() => {
-        console.log(props.showLeagueCreationModal);
         if (props.showLeagueCreationModal) setModalIsOpen(true);
         else setModalIsOpen(false);
     }, [props.showLeagueCreationModal]);
@@ -56,22 +56,101 @@ function LeagueCreationModal(props) {
         props.setShowLeagueCreationModal(false);
     }
 
+    const rosterSettings = {"Forwards Allowed" : 6, "Defensemen Allowed": 4, "Goalies Allowed": 2};
+    const pointsSettings = {"Goals" : 3, "Assists" : 2, "Penalty Minutes" : 0.5, "Shots" : 0.1, "Hits" : 0.5, "Power Play Points" : 1, "Short Handed Points" : 2, "Blocks" : 0.5, "Wins" : 3, "Losses" : 0, "Overtime Losses" : 1, "Saves" : 0.2, "Shutouts" : 3, "Goals Against" : -1};
+
+    function RosterSettingsForms() {
+        return (
+            <div>
+                {Object.keys(rosterSettings).map((setting, index) => (
+                    <div key={index}>
+                        <TextField
+                        id={setting}
+                        label={setting}
+                        name={setting}
+                        type="number"
+                        required
+                        inputProps={{ min: 0, max: 10, step: 1 }}
+                        defaultValue={rosterSettings[setting]}
+                        sx={{ my: 1, width: '30%' }}
+                        />
+                        </div>
+                ))}
+            </div>
+        )
+    }
+
+    function PointsSettingsForms() {
+        return (
+            <div>
+                {Object.keys(pointsSettings).map((setting, index) => (
+                    <div key={index}>
+                        <TextField
+                        id={setting}
+                        label={setting}
+                        name={setting}
+                        type="number"
+                        required
+                        inputProps={{ min: -10, max: 10, step: 0.1 }}
+                        defaultValue={pointsSettings[setting]}
+                        sx={{ my: 1, width: '30%' }}
+                        />
+                        </div>
+                ))}
+            </div>
+        )
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log(data);
-        let leagueName = data.get('leagueName')+"";
-        if(leagueName.length < 1) {
-            alert("Please enter a league name.");
-            return;
-        }
 
+        // post to api
+        const leagueName = data.get('Name');
+        const rosterSettings = {"Forwards Allowed" : data.get('Forwards Allowed'), "Defensemen Allowed": data.get('Defensemen Allowed'), "Goalies Allowed": data.get('Goalies Allowed')};
+        const pointsSettings = {"Goals" : data.get('Goals'), "Assists" : data.get('Assists'), "Penalty Minutes" : data.get('Penalty Minutes'), "Shots" : data.get('Shots'), "Hits" : data.get('Hits'), "Power Play Points" : data.get('Power Play Points'), "Short Handed Points" : data.get('Short Handed Points'), "Blocks" : data.get('Blocks'), "Wins" : data.get('Wins'), "Losses" : data.get('Losses'), "Overtime Losses" : data.get('Overtime Losses'), "Saves" : data.get('Saves'), "Shutouts" : data.get('Shutouts'), "Goals Against" : data.get('Goals Against')};
+        fetch(`${Routes.POST.CREATELEAGUE}/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "Name": leagueName,
+                "NumForwards": rosterSettings["Forwards Allowed"],
+                "NumDefensemen": rosterSettings["Defensemen Allowed"],
+                "NumGoalies": rosterSettings["Goalies Allowed"],
+                "Goals": pointsSettings["Goals"],
+                "Assists": pointsSettings["Assists"],
+                "Pim": pointsSettings["Penalty Minutes"],
+                "Shots": pointsSettings["Shots"],
+                "Hits": pointsSettings["Hits"],
+                "PowerPlayPoints": pointsSettings["Power Play Points"],
+                "ShortHandedPoints": pointsSettings["Short Handed Points"],
+                "Blocked": pointsSettings["Blocks"],
+                "Wins": pointsSettings["Wins"],
+                "Losses": pointsSettings["Losses"],
+                "Ot": pointsSettings["Overtime Losses"],
+                "Saves": pointsSettings["Saves"],
+                "Shutouts": pointsSettings["Shutouts"],
+                "GoalsAgainst": pointsSettings["Goals Against"]
+        }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        // close modal
+        closeModal();
     }
 
     return (
         <div>
             <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={Styles}>
-                <h2>Create League</h2>
+                <div style={{borderBottom: "solid", borderColor: "lightgray", marginBottom: "3%"}}><h2>Create League</h2></div>
                 <Box component="form"
                 sx={{
                     my: 1,
@@ -81,25 +160,32 @@ function LeagueCreationModal(props) {
                     justifyContent: 'flex-start',
                     alignItems: 'flex-start',
                 }}
+                onSubmit={handleSubmit}
                 autoComplete="off"
                 >
                     <div className='league-form-container'>
-                        <div>League Name</div>
+                        <h5>League Name</h5>
+
                         <TextField
                         required
                         fullWidth
-                        id="outlined-required"
-                        label="Required"
-                        defaultValue="Hello World"
+                        id="Name"
+                        label="Name"
+                        name="Name"
+                        sx = {{ mb: 3, width: '100%' }}
+                        InputProps={{
+                            inputMode: "numeric",
+                            pattern: "^[a-zA-Z0-9]*$",
+                            minLength: 3,
+                            maxLength: 30,
+                          }}
                         />
-                        <TextField
-                        id="outlined-number"
-                        label="Number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        />
+
+                        <h5>Roster Settings</h5>
+                        <RosterSettingsForms />
+
+                        <h5>Points Settings</h5>
+                        <PointsSettingsForms />
                     <Button
                         type="submit"
                         fullWidth
@@ -118,7 +204,7 @@ function LeagueCreationModal(props) {
 const Styles = {
     content: {
         
-        width: '50%',
+        width: '40%',
         height: '70%',
         top: '50%',
         left: '50%',

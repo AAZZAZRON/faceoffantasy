8ad3e68954093144ddefe78a1d6fe580e2ba94f8
Max Sun <maxsun715@gmail.com>
2023-04-09 11:47:45 -0400

    add create team endpoint

--- backend/team/views.py
+++ backend/team/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import *
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from ..league.models import League
 
 # Create your views here.
 
 class TeamView(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
+@csrf_exempt
+def create_team(request):
+    message = {'message': '', 'success': False}
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        if (not data['name']):
+            message['message'] = 'Team name is required'
+            return JsonResponse(message)
+        league = League.objects.filter(id=data['league']).first()
+        if not league:
+            message['message'] = 'League does not exist'
+            return JsonResponse(message)
+        if Team.objects.filter(league=league).filter(name=data['name']):
+            message['message'] = 'A team with this name already exists'
+            return JsonResponse(message)
+        team = Team.objects.create(teamName=data['name'])
+        team.league = league
+        team.save()
+        message['message'] = 'Team created successfully'
+        message['success'] = True
+    return JsonResponse(message)
